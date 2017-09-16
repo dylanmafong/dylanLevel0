c2c82d8fafb7769844b11dd846d99688ee84b605
dylanmafong <league@WTS-iMac-11.attlocal.net>
2017-09-15 17:06:53 -0700

    GuessingPi and NumberSorter

---
+++ src/NumberSorter.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class NumberSorter {
+	public static void main(String[] args) {
+		String NumberOne =JOptionPane.showInputDialog("give me your first number");
+		String NumberTwo =JOptionPane.showInputDialog("give me your second number");
+		String NumberThree =JOptionPane.showInputDialog("give me your third number number");
+		
+		
+		
+		int int1=Integer.parseInt(NumberOne);
+		int int2=Integer.parseInt(NumberTwo);
+		int int3=Integer.parseInt(NumberThree);
+	}
+
+}
