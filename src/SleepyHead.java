0e45e1967e372a6609bc2c498302816e15deb326
codeavenger1 <league@WTS10.attlocal.net>
2017-09-29 16:56:13 -0700

    CircleRing

--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -5,20 +5,33 @@ public class SleepyHead {
 
 	public static void main(String[] args) {
 
-		boolean isWeekday, isVacation;
+		boolean isWeekday, isVacation = false;
 		int x = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "", JOptionPane.YES_NO_OPTION);
 		if (x == 0) {
 			isWeekday = true;
 			int y = JOptionPane.showConfirmDialog(null, "Is it a vacation?", "", JOptionPane.YES_NO_OPTION);
-			if (x == 0) {
+			if (y == 0) {
 				isVacation = true;
-				JOptionPane.showMessageDialog(null, "get up lazy bones");
 
 			} else {
-				JOptionPane.showMessageDialog(null, "Sleep in");
+				isVacation = false;
+				
 			}
 		} else {
 			isWeekday = false;
+		}
+		 if(isWeekday&&!isVacation) {
+		JOptionPane.showMessageDialog(null, "get up lazy bones");
+		 
+		 }
+		 
+		 
+		 
+		if(isVacation&&isWeekday) {
+		JOptionPane.showMessageDialog(null, "Sleep in");
+		}
+		if(!isWeekday) {
+			JOptionPane.showMessageDialog(null, "Sleep in");
 		}
 		/*
 		 * Ask the user for these values using JOptionPane.showConfirmDialog(null,
@@ -31,4 +44,4 @@ public class SleepyHead {
 		 * in”.
 		 */
 	}
-}
+	}
