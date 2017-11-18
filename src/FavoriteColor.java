b02dbc5bb1a95085757b4ee98e4cfc0e9ca47582
GabrielZ1 <League@AdministorsiMac.attlocal.net>
2017-11-17 16:55:58 -0800

    LjawdfhlBAWER
    ASDFASDFASDFASDFASEFsdrtgwr456n a yhgrefvsdxycgb

---
+++ src/FavoriteColor.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class FavoriteColor {
+public static void main(String[] args) {
+	String color =JOptionPane.showInputDialog( "what is your favorite color");
+	System.out.println("My favorite color is also " + color);
+			
+}
+}
